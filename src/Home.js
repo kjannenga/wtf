46ea084fd01257b7f74bb49ab505cba084804631
Taylor McArthur <taydm.erek@gmail.com>
2017-05-02 09:38:26 -0700

    landingpage

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './App.css'
 
 export default React.createClass ({
   render() {
     return (
-    	<div>
-      	<h1> home </h1>
-      	<Link to='/criteria'><div>dirty</div></Link>
-      	<Link to='/criteria'><div>clean</div></Link>
+    	<div className="mainBody">
+    		<div className="warningHeader">
+	      	<img className="warningImg" src="https://s-media-cache-ak0.pinimg.com/originals/bf/0c/cd/bf0ccdce53d7ee3612a877f57f51ea84.jpg" alt="explicit-content" height='350'/>
+	      	</div>
+	      		<div className="warningContent">
+	      	<h2>WARNING! This website may contain content that is deemed inappropriate by parents, children, and 3/4 of the population of the state of Utah. Please select your viewing preference below.</h2>
+   				</div>
+	      	<Link to='/criteria'><div className="dirty">dirty</div></Link>
+	      	<Link to='/criteria'><div className="clean">clean</div></Link>
+	      
       </div>
     );
   }
