7d3778bf9cbf2430044c1011ca9527884f471eb6
brett poarch <brett.poarch@gmail.com>
2017-05-08 09:08:54 -0700

    removed duplicate on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -20,31 +20,8 @@ handleClick(e) {
       		<div className="logoHeader">
             <img className="logo" src={logo} alt="logo" />
   	      </div>
-
-          <div className="buttonDiv">
-            
-            <div className="linkButtons">
-              <div id="dirty">
-    	      	  <Link id="dirty" to='/criteria'>dirty</Link>
-              </div>
-            </div>
-              
-            <img className="warningImg" src="http://i.imgur.com/29Bnqcb.png" alt="explicit-content" height='450'/>
-            
-            <div className="linkButtons">
-              <div id="clean">
-    	      	  <Link to='/criteria'>clean</Link>
-              </div>
-            </div>
-            
-  	      </div>
-          
-          <div className="bottomContainer">
-            <div className="warningContent">
-              <h2 className="expContent">WARNING! This website may contain content that is deemed inappropriate by parents, children, and 3/4 of the population of the state of Utah. Please select your viewing preference below.</h2>
-            </div>
-          </div>
         </div>
+
 
         <div className="bottomGraphic">
           <div className="buttonDiv">
