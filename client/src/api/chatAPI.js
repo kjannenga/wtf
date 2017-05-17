5869ad79f7f70ac3749302998dde8fc8cee70447
KrystineJannenga <kjannenga1@gmail.com>
2017-05-16 21:56:24 -0700

    styles going crazy

--- client/src/api/chatAPI.js
+++ client/src/api/chatAPI.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import store from '../store'
 
 // const socket = io.connect('http://192.168.0.29:3001')
-const socket = io.connect('http://10.68.0.132:3001')
+const socket = io.connect('http://192.168.0.8:3001')
 
 //const socket = io.connect('http://:3001')
 
