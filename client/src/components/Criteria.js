34f71d1660fffeba3d3d2fdbc70af1eb2e57e1fa
Taylor McArthur <taydm.erek@gmail.com>
2017-05-16 17:16:46 -0700

    pulling

--- client/src/components/Criteria.js
+++ client/src/components/Criteria.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import logo from '../assets/logo.png'
+import logo from '../assets/wtf.png'
 import {connect} from 'react-redux'
 import './css/criteria.css'
 import {getRestaurants} from '../api/wtf'
