64b12a358acc19d1b6e5f8cbab17d8e788b2264f
Taylor McArthur <taydm.erek@gmail.com>
2017-05-16 17:45:30 -0700

    added alert message to yes button

--- client/src/components/Criteria.js
+++ client/src/components/Criteria.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-import logo from '../assets/wtf.png'
+import logoo from '../assets/wtf.png'
 
 import {connect} from 'react-redux'
 import './css/criteria.css'
