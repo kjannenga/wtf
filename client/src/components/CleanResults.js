dd468c42b26f4fc69bb063f789ed4d091544a673
KrystineJannenga <kjannenga1@gmail.com>
2017-05-16 17:00:37 -0700

    finished criteria

--- client/src/components/CleanResults.js
+++ client/src/components/CleanResults.js
@@ -42,14 +42,20 @@ render(){
         <div>
           <img className="resultsLogo" src={logoo} alt="logo" />
         </div>
-        <div >
+
+        <div className="resultsButton">
+          <Link to='/yes'><button>Sounds Good</button></Link><br/>
+          <button type='submit' onClick={this.handleClick}>Eww Gross</button>
+        </div>
+
+        <div>
         <ul  className="resultsList">
             {this.state.restaurant.map(function(value){
               return <li key={'id' + value.id}>
                        <div className="resultName">{value.name}</div>
                        <div>
                          <iframe
-                            width="1200"
+                            width="1000"
                             height="375"
                             frameBorder="0"
                             src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyDiZLjRIWsbb_SPwIXRxPGWyW-ILTBIqEo&origin=The+Iron+Yard,Las+Vegas&destination=${value.coordinates.latitude + ',' + value.coordinates.longitude}`} allowFullScreen>
@@ -57,19 +63,18 @@ render(){
                        </div>
                        <div className="resultsInfo">
                         
-                         <div>Rating<div>{value.rating + '/5'}</div></div>
+                         <div><span className="resultheading">Rating</span><div>{value.rating + '/5'}</div></div>
                          
-                         <div>Address<div>{value.location.address1}</div>
+                         <div><span className="resultheading">Address</span><div>{value.location.address1}</div>
                        <div>{value.location.city + ', ' + value.location.state + ' ' + value.location.zip_code}</div></div>
 
-                         <div>Image<div><img className='resultImage' src={value.image_url} alt='restaurant' /></div></div>
+                         <div><div><span className="resultheading">Image</span><div><img className='resultImage' src={value.image_url} alt='restaurant' /></div></div></div>
                         </div>
                      </li>
              })}
           </ul>
           </div>
-        <Link to='/yes'><button>F Yes</button></Link>
-        <button type='submit' onClick={this.handleClick}>F this</button>
+         
       </div>
     )
     
--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {getRestaurant} from '../api/wtf'
-//import {populateRestaurants} from '../api/chatAPI'
 import logoo from '../assets/wtf.png'
 import './css/results.css'
 
@@ -13,14 +12,13 @@ const Restaurant = React.createClass({
 getInitialState(){
   return {
       restaurant: [],
-      businesses:'',
       restId: '',
     }
   },
 
 
 componentWillReceiveProps(props) {
-  //console.log(props.info.businesses)
+  //console.log(props, 'a')
   this.setState({
     restaurant: [props.info.businesses[id]]
   })
@@ -40,46 +38,51 @@ handleClick(e) {
 
 render(){
     return (
-    	<div>
-      	<div>
+      <div>
+        <div>
           <img className="resultsLogo" src={logoo} alt="logo" />
         </div>
-        <div >
-      	<ul  className="resultsList">
+
+        <div className="resultsButton">
+          <Link to='/yes'><button>Sounds Good</button></Link><br/>
+          <button type='submit' onClick={this.handleClick}>Eww Gross</button>
+        </div>
+
+        <div>
+        <ul  className="resultsList">
             {this.state.restaurant.map(function(value){
               return <li key={'id' + value.id}>
                        <div className="resultName">{value.name}</div>
                        <div>
                          <iframe
-                            width="60%"
-                            height="90%"
+                            width="1000"
+                            height="375"
                             frameBorder="0"
                             src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyDiZLjRIWsbb_SPwIXRxPGWyW-ILTBIqEo&origin=The+Iron+Yard,Las+Vegas&destination=${value.coordinates.latitude + ',' + value.coordinates.longitude}`} allowFullScreen>
                          </iframe>
                        </div>
                        <div className="resultsInfo">
                         
-                         <div>Rating<div>{value.rating + '/5'}</div></div>
+                         <div><span className="resultheading">Rating</span><div>{value.rating + '/5'}</div></div>
                          
-                         <div>Address<div>{value.location.address1}</div>
+                         <div><span className="resultheading">Address</span><div>{value.location.address1}</div>
                        <div>{value.location.city + ', ' + value.location.state + ' ' + value.location.zip_code}</div></div>
 
-                         <div>Image<div><img className='resultImage' src={value.image_url} alt='restaurant' /></div></div>
+                         <div><div><span className="resultheading">Image</span><div><img className='resultImage' src={value.image_url} alt='restaurant' /></div></div></div>
                         </div>
                      </li>
              })}
           </ul>
           </div>
-    		<Link to='/yes'><button>Fuck Yes</button></Link>
-    		<button type='submit' onClick={this.handleClick}>Fuck this</button>
+         
       </div>
     )
-		
-	}
+    
+  }
 })
 
 function mapStateToProps(state){
-	return {...state.restaurantReducer}
+  return {...state.restaurantReducer}
 }
 
 export default connect(mapStateToProps)(Restaurant)
